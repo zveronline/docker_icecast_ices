404433eb4aed2be191780f9c21dad9a20af221e7
root <root@chilloutplanet.ru>
2018-06-12 23:52:47 +0000

    modified:   LICENSE 	modified:   README.md 	deleted:    audio/tracks/03-Dreamstalker-Spirals-live-edit-MASTER.mp3 	deleted:    start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#chmod -R 777 /stream/*
-#find /stream/tracks -type f -name \*.mp3 > /stream/playlist.txt
-#cat /stream/playlist.txt
-icecast2 -c icecast.xml -b
-sleep 5
-ices -c ices.xml -v 
-while true; do sleep 10; done
